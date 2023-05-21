82a9dcbdd0e8a1f7f71cb6cf19362fff20b109ab
md:robin mia <mdrobinmiaro1@gmail.com>
2023-05-21 09:37:05 +0600

    barbie dolls done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -29,11 +29,49 @@ async function run() {
     await client.connect();
 
     const serviceCollection= client.db('babydolls').collection('servicesdoll');
+    const servicesBarbieCollection= client.db('barbiedolls').collection('servicesBarbie');
+ 
 
+
+    // servicesdoll
     app.get('/servicesdoll', async(req, res)=>{
         const result= await serviceCollection.find().toArray();
         res.send(result)
     })
+
+    app.get('/servicesdoll/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+
+        const options = {
+          
+          // Include only the `title` and `imdb` fields in the returned document
+          projection: {Picture: 1, category: 1, Rating: 1, details: 1, price: 1, },
+        };
+
+        const result = await serviceCollection.findOne(query, options)
+        res.send(result)
+    })
+
+    // servicesBarbie
+    app.get('/servicesBarbie', async(req, res)=>{
+      const result= await servicesBarbieCollection.find().toArray();
+      res.send(result)
+  })
+
+  app.get('/servicesBarbie/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+
+      const options = {
+        
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {Picture: 1, category: 1, Rating: 1, details: 1, price: 1, },
+      };
+
+      const result = await servicesBarbieCollection.findOne(query, options)
+      res.send(result)
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
