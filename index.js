7d17bb94205fce99318402bc6482186f4cffc7d5
md:robin mia <mdrobinmiaro1@gmail.com>
2023-05-21 20:47:47 +0600

    my toy data added

--- index.js
+++ index.js
@@ -91,6 +91,10 @@ async function run() {
       res.send(result)
     })
 
+    // 
+
+
+
     app.get('/servicesBarbie/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
